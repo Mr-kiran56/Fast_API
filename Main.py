36605b34181124ea8de76ca6a9da41390388d9c8
Mr-kiran56 <kiranpunna815@gmail.com>
2025-09-07 21:48:29 +0530

    commit

--- Main.py
+++ Main.py
@@ -5,3 +5,15 @@ app=FastAPI()
 @app.get('/')
 def Index():
     return {"dataa":{"kiran":"Btech","Age":20}}
+@app.get("/blog/tranform")
+def blog():
+    return "thiis is blog"
+@app.get("/blog/{id}")
+def blog(id:int ):
+    return {"data":{id}}
+
+@app.get("/blog/{id}/number")
+def blog():
+    return {"data":{1,2,3,4,5}}
+
+
